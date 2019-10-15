140c5c63c8e0077bc4de0e33f66f9b3f168ef5be
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-15 19:40:19 +0200

    add details with javascript (play/stop)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.getElementById('name-song').innerHTML = "Bohemian Rhapsody"
 
 document.getElementById('name-group').innerHTML = "Queen"
+
+
+function play(){
+  document.getElementById('bars').style.opacity = "1";
+}
+function stop(){
+  document.getElementById('bars').style.opacity = "0";
+}
