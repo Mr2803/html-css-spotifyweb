d771a778393c8221716c03967652a40905d4d7c2
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-15 20:17:04 +0200

    add details in navbar-top-orizzontale (the first line is visible when the page is loaded. Each line disappears when another link is clicked and reappears on the clicked link)

--- js/script.js
+++ js/script.js
@@ -3,6 +3,58 @@ document.getElementById('name-song').innerHTML = "Bohemian Rhapsody"
 document.getElementById('name-group').innerHTML = "Queen"
 
 
+
+function link1(){
+  document.getElementById('first-line').style.visibility = "visible";
+  document.getElementById('second-line').style.visibility = "hidden";
+  document.getElementById('third-line').style.visibility = "hidden";
+  document.getElementById('fourth-line').style.visibility = "hidden";
+  document.getElementById('fifth-line').style.visibility = "hidden";
+  document.getElementById('sixth-line').style.visibility = "hidden";
+}
+
+function link2(){
+  document.getElementById('first-line').style.visibility = "hidden";
+  document.getElementById('second-line').style.visibility = "visible";
+  document.getElementById('third-line').style.visibility = "hidden";
+  document.getElementById('fourth-line').style.visibility = "hidden";
+  document.getElementById('fifth-line').style.visibility = "hidden";
+  document.getElementById('sixth-line').style.visibility = "hidden";
+}
+function link3(){
+  document.getElementById('first-line').style.visibility = "hidden";
+  document.getElementById('second-line').style.visibility = "hidden";
+  document.getElementById('third-line').style.visibility = "visible";
+  document.getElementById('fourth-line').style.visibility = "hidden";
+  document.getElementById('fifth-line').style.visibility = "hidden";
+  document.getElementById('sixth-line').style.visibility = "hidden";
+}
+function link4(){
+  document.getElementById('first-line').style.visibility = "hidden";
+  document.getElementById('second-line').style.visibility = "hidden";
+  document.getElementById('third-line').style.visibility = "hidden";
+  document.getElementById('fourth-line').style.visibility = "visible";
+  document.getElementById('fifth-line').style.visibility = "hidden";
+  document.getElementById('sixth-line').style.visibility = "hidden";
+}
+function link5(){
+  document.getElementById('first-line').style.visibility = "hidden";
+  document.getElementById('second-line').style.visibility = "hidden";
+  document.getElementById('third-line').style.visibility = "hidden";
+  document.getElementById('fourth-line').style.visibility = "hidden";
+  document.getElementById('fifth-line').style.visibility = "visible";
+  document.getElementById('sixth-line').style.visibility = "hidden";
+}
+function link6(){
+  document.getElementById('first-line').style.visibility = "hidden";
+  document.getElementById('second-line').style.visibility = "hidden";
+  document.getElementById('third-line').style.visibility = "hidden";
+  document.getElementById('fourth-line').style.visibility = "hidden";
+  document.getElementById('fifth-line').style.visibility = "hidden";
+  document.getElementById('sixth-line').style.visibility = "visible";
+}
+
+
 function play(){
   document.getElementById('bars').style.opacity = "1";
 }
